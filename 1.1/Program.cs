5499fc215d04ded78549aab7a67f7ffd979f6f1f
Opex2 <makaruev@gmail.com>
2025-02-18 12:39:48 +0800

    Добавьте файлы проекта.

---
+++ 1.1/Program.cs
@@ -0,0 +1,94 @@
+﻿namespace T1_1;
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int money = Input();
+        Logic logic = new Logic(money);
+        Output(logic);
+    }
+
+    static int Input()
+    {
+        Console.Write("Введите сумму в копейках: ");
+        return Convert.ToInt32(Console.ReadLine());
+    }
+
+    static void Output(Logic logic)
+    {
+        if (logic.GetRub() == 0)
+        {
+            Console.WriteLine($"{logic.GetKop()} {logic.KopStr()}");
+        }
+        else if (logic.GetKop() == 0)
+        {
+            Console.WriteLine($"{logic.GetRub()} {logic.RubStr()}");
+        }
+        else
+        {
+            Console.WriteLine($"{logic.GetRub()} {logic.RubStr()} {logic.GetKop()} {logic.KopStr()}");
+        }
+    }
+}
+
+public class Logic
+{
+    private int rub;
+    private int kop;
+
+    public Logic(int money)
+    {
+        this.rub = money / 100;
+        this.kop = money % 100;
+    }
+
+    public int GetRub()
+    {
+        return rub;
+    }
+
+    public int GetKop()
+    {
+        return kop;
+    }
+
+    public String RubStr()
+    {
+        if (rub % 100 / 10 == 1)
+        {
+            return "рублей";
+        } else if (rub % 10 == 1)
+        {
+            return "рубль";
+        } else if (rub % 10 is 2 or 3 or 4)
+        {
+            return "рубля";
+        } else
+        {
+            return "рублей";
+        }
+    }
+
+    public String KopStr()
+    {
+        if (kop % 100 / 10 == 1)
+        {
+            return "копеек";
+        }
+        else if (kop % 10 == 1)
+        {
+            return "копейка";
+        }
+        else if (kop % 10 is 2 or 3 or 4)
+        {
+            return "копейки";
+        }
+        else
+        {
+            return "копеек";
+        }
+    }
+}
---
+++ TestsForLab1/Test1.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestPlatform.TestHost;
+
+namespace T1_1.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void Rub1()
+    {
+        var logic = new Logic(100);
+        Assert.AreEqual("рубль", logic.RubStr());
+    }
+    [TestMethod]
+    public void Rub234()
+    {
+        var logic = new Logic(300);
+        Assert.AreEqual("рубля", logic.RubStr());
+    }
+    [TestMethod]
+    public void Rub()
+    {
+        var logic = new Logic(10000);
+        Assert.AreEqual("рублей", logic.RubStr());
+    }
+    [TestMethod]
+    public void Kop1()
+    {
+        var logic = new Logic(1);
+        Assert.AreEqual("копейка", logic.KopStr());
+    }
+    [TestMethod]
+    public void Kop234()
+    {
+        var logic = new Logic(3);
+        Assert.AreEqual("копейки", logic.KopStr());
+    }
+    [TestMethod]
+    public void Kop()
+    {
+        var logic = new Logic(10);
+        Assert.AreEqual("копеек", logic.KopStr());
+    }
+}
